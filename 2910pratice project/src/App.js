78766f407bba69f5a08685df5cd7d6cd6850a921
excellingtech <komalkm012@gmail.com>
2024-10-29 09:27:44 +0530

    add todo app2910projec

---
+++ 2910pratice project/src/App.js
@@ -0,0 +1,21 @@
+import "./index.css"
+
+
+const App = () => {
+  return (
+    <div>
+      <input type="text"></input>
+      <button>Add</button>
+     <ul>
+      <li>sdss</li>
+      <li>sdss</li>
+      <li>sdss</li>
+      <li>sdss</li>
+      <li>sdss</li>
+     </ul>
+    </div>
+  
+  )
+};
+
+export default App;
---
+++ 2910pratice project/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDom from "react-dom/client";
+import App from "./App";
+
+  const ReactRoot=ReactDom.createRoot(document.getElementById("root"));
+ReactRoot.render(<App/>);
+
